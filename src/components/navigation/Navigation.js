d840d6724101cafb99a695e0b85082de3524bed6
Elena <podolyak365@gmail.com>
2022-10-26 23:26:11 +0300

    nav button

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,49 +1,126 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
+
+import homeSvg from '../../img/nav/home.svg';
+import personSvg from '../../img/nav/person.svg';
+import skillsSvg from '../../img/nav/skills.svg';
+import myWorkSvg from '../../img/nav/myWork.svg';
+import contactsSvg from '../../img/nav/contacts.svg';
+
 const Navigation = () => {
   return (
     <NavContainer>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+      {/* <NavBattonsSection> */}
+      <NavLinkButton
+        // style={({ isActive }) => ({ color: isActive ? 'green' : 'black' })}
         to="home"
       >
-        Home
-      </NavLink>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+        <IconNav src={homeSvg} alt="icon home" />
+        <SpanNavText>Home</SpanNavText>
+      </NavLinkButton>
+      <NavLinkButton
+        // style={({ isActive }) => ({
+        //   color: isActive ? 'green' : 'black',
+        // })}
         to="about"
       >
-        About
-      </NavLink>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+        <IconNav src={personSvg} alt="icon home" />
+        {/* About */}
+      </NavLinkButton>
+      <NavLinkButton
+        // style={({ isActive }) => ({ color: isActive ? 'green' : 'black' })}
         to="skills"
       >
-        Skills
-      </NavLink>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+        <IconNav src={skillsSvg} alt="icon home" />
+        {/* Skills */}
+      </NavLinkButton>
+      {/* </NavBattonsSection> */}
+      {/* <NavBattonsSection> */}
+      <NavLinkButton
+        // style={({ isActive }) => ({ color: isActive ? 'green' : 'black' })}
         to="mywork"
       >
-        MyAbout
-      </NavLink>
-      <NavLink
-        style={({ isActive }) => ({ color: isActive ? 'green' : 'blue' })}
+        <IconNav src={myWorkSvg} alt="icon home" />
+        {/* My works */}
+      </NavLinkButton>
+      <NavLinkButton
+        // style={({ isActive }) => ({ color: isActive ? 'green' : 'black' })}
         to="contact"
       >
-        Contact
-      </NavLink>
+        <IconNav src={contactsSvg} alt="icon home" />
+        {/* Contact */}
+      </NavLinkButton>
+      {/* </NavBattonsSection> */}
     </NavContainer>
   );
 };
 export default Navigation;
 
 const NavContainer = styled.nav`
-  width: 400px;
+  /* font-family: 'Jacques Francois Shadow'; */
+  width: 100px;
+  padding: 20px;
   display: flex;
-  justify-content: space-between;
+  flex-wrap: wrap;
+  border-radius: 20px;
+  background-color: #9370db;
   margin-left: auto;
+  text-transform: uppercase;
 `;
-// const NavLinkTo = styled('Navlink')`
-//   padding: 10px;
+// const NavBattonsSection = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   :not(:first-child) {
+//     margin-top: 10px;
+//   }
 // `;
+const NavLinkButton = styled(NavLink)`
+  padding: 20px;
+  width: 60px;
+  justify-content: center;
+  text-align: center;
+  text-decoration: none;
+  border-radius: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+
+  :not(:first-child) {
+    margin-top: 20px;
+  }
+  :active,
+  :focus,
+  :hover {
+    border-radius: 20px;
+    background-color: #7b49dd;
+
+    /* transform: translateY(-17px);
+    transition: 300ms; */
+  }
+`;
+const IconNav = styled.img`
+  width: 46px;
+  height: 46px;
+  :hover + span {
+    display: inline-block;
+  }
+
+  /* :hover {
+    content: 'Home';
+  } */
+  /* fill: #ffffff;
+  stroke: #ffffff; */
+`;
+const SpanNavText = styled.span`
+  display: none;
+  position: absolute;
+  font-size: 12px;
+  font-weight: bold;
+  top: 50%;
+  /* margin-top: 10px; */
+  /* color: #000000; */
+  color: #ffffff;
+  :hover {
+    display: inline-block;
+  }
+`;
